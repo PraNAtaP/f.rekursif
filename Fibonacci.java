741568e92752a22717b0851db03a68f35fd4ee11
PraNAtaP <pranatapu08@gmail.com>
2024-11-29 10:02:05 +0700

    Tugas No.4

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static int jumlahPasangan(int bulan) {
+        if (bulan == 1 || bulan == 2) {
+            return 1;
+        }   
+        return jumlahPasangan(bulan - 1) + jumlahPasangan(bulan - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan bulan ke-n untuk mengetahui jumlah pasangan marmut: ");
+        int bulan = scanner.nextInt();
+
+        if (bulan <= 0) {
+            System.out.println("Bulan harus lebih besar dari 0!");
+        } else {
+            int totalPasangan = jumlahPasangan(bulan);
+            System.out.println("Jumlah pasangan marmut pada akhir bulan ke-" + bulan + " adalah: " + totalPasangan);
+        }
+
+        scanner.close();
+    }
+}
